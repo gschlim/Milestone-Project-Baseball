9913e0f9a5eee278bdc083798db7b7b4e6116ff6
Titty Tooty <grant.schlimgen@gmail.com>
2022-07-06 21:45:25 -0500

    styling changes, added replay button

--- fieldwork.js
+++ fieldwork.js
@@ -2,23 +2,24 @@ function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'absolute'
-    object.style.zIndex = 3
+    object.style.zIndex = 1
     object.style.left = left + 'px'
     object.style.bottom = bottom + 'px'
     document.body.append(object)
     return object
 }
+//function NewImage is a tweaked version of the Web Game part 1 activity from class, which I am using to place the two teams precisely where I want them on the field background//
+newImage('small_red_cap.png', 420, -600)
+newImage('small_red_cap.png', 555, -465)
+newImage('small_red_cap.png', 880, -430)
+newImage('small_red_cap.png', 590, -140)
+newImage('small_red_cap.png', 800, -220)
+newImage('small_red_cap.png', 500, -310)
+newImage('small_red_cap.png', 620, -310)
+newImage('small_red_cap.png', 720, -390)
+newImage('small_red_cap.png', 720, -540)
+newImage('small_blue_cap.png', 455, -565)
+newImage('small_blue_cap.png', 665, -565)
+newImage('small_blue_cap.png', 665, -355)
+newImage('small_blue_cap.png', 455, -355)
 
-newImage('small_red_cap.png', 420, -400)
-newImage('small_red_cap.png', 555, -265)
-newImage('small_red_cap.png', 880, -230)
-newImage('small_red_cap.png', 590, 60)
-newImage('small_red_cap.png', 800, -20)
-newImage('small_red_cap.png', 500, -110)
-newImage('small_red_cap.png', 620, -110)
-newImage('small_red_cap.png', 720, -190)
-newImage('small_red_cap.png', 720, -340)
-newImage('small_blue_cap.png', 455, -365)
-newImage('small_blue_cap.png', 665, -365)
-newImage('small_blue_cap.png', 665, -155)
-newImage('small_blue_cap.png', 455, -155)
--- nbscript.js
+++ nbscript.js
@@ -3,88 +3,89 @@ function showInput() {
         document.getElementById("team_name").value;
 }
 //One of the first Google searches I made was how to instantly populate a text box from a form without console.log, and showInput is an example function from w3schools modified for the variables in my html file//
-function heresThePitch(){
-    document.getElementById('pitch').value = 
+function heresThePitch() {
+    document.getElementById('pitch').value =
         (Math.floor(Math.random() * 100)) + 1;
     document.getElementById('gameplay').disabled = true
 }
 
-function answer(){
+function answer() {
     var swingnum = document.getElementById('swing').value
     var pitchnum = document.getElementById('pitch').value
     var pitchdiff = Math.abs(swingnum - pitchnum)
     if (pitchdiff > 50) {
         document.getElementById('result').value = 100 - pitchdiff
     } else {
-        document.getElementById('result').value = pitchdiff  
+        document.getElementById('result').value = pitchdiff
     }
 }
-    
-function yerout(){
-    document.getElementById('howmanyouts').innerHTML = "X X X"
-    
-    var div = document.createElement("div");
-    div.style.width = "400px";
-    div.style.height = "50px";
-    div.style.zIndex = "2";
-    div.style.background = "red";
-    div.style.color = "white";
-    div.style.position = "absolute";
-    div.style.top = "300px";
-    div.style.left = "100px";
-    div.innerHTML = "Monsters Win";
-    div.style.fontSize = "40px";
-    div.style.textAlign = "center";
 
-    document.getElementById("main").appendChild(div);
+var red = document.createElement("div");
+red.style.width = "400px";
+red.style.height = "50px";
+red.style.zIndex = "2";
+red.style.background = "red";
+red.style.color = "white";
+red.style.position = "absolute";
+red.style.top = "305px";
+red.style.left = "100px";
+red.innerHTML = "Monsters Win";
+red.style.fontSize = "40px";
+red.style.textAlign = "center";
+
+let blu;
+
+function yerout() {
+    document.getElementById('howmanyouts').innerHTML = "X X X"
+    document.getElementById("main").appendChild(red);
 }
 
-function youwin(){
+function youwin() {
     document.getElementById('homescore').innerHTML = "4"
 
-    var div = document.createElement("div");
-    div.style.width = "400px";
-    div.style.height = "50px";
-    div.style.zIndex = "2";
-    div.style.background = "blue"
-    div.style.color = "white";
-    div.style.position = "absolute";
-    div.style.top = "300px";
-    div.style.left = "100px";
-    div.innerHTML = document.getElementById("team_name").value + " Win!!!";
-    div.style.fontSize = "40px";
-    div.style.textAlign = "center";
+    blu = document.createElement("div");
+    blu.style.width = "400px";
+    blu.style.height = "50px";
+    blu.style.zIndex = "2";
+    blu.style.background = "blue";
+    blu.style.color = "white";
+    blu.style.position = "absolute";
+    blu.style.top = "305px";
+    blu.style.left = "100px";
+    blu.innerHTML = document.getElementById('team_name').value + " Win!!!";
+    blu.style.fontSize = "40px";
+    blu.style.textAlign = "center";
 
-    document.getElementById("main").appendChild(div);
+    document.getElementById("main").appendChild(blu);
 }
 
-function dingdong(){
+function dingdong() {
     document.getElementById('homescore').innerHTML = "6"
 
-    var div = document.createElement("div");
-    div.style.width = "400px";
-    div.style.height = "50px";
-    div.style.zIndex = "2";
-    div.style.background = "blue"
-    div.style.color = "white";
-    div.style.position = "absolute";
-    div.style.top = "300px";
-    div.style.left = "100px";
-    div.innerHTML = document.getElementById("team_name").value + " Win!!!";
-    div.style.fontSize = "40px";
-    div.style.textAlign = "center";
+    var blu = document.createElement("div");
+    blu.style.width = "400px";
+    blu.style.height = "50px";
+    blu.style.zIndex = "2";
+    blu.style.background = "blue";
+    blu.style.color = "white";
+    blu.style.position = "absolute";
+    blu.style.top = "305px";
+    blu.style.left = "100px";
+    blu.innerHTML = document.getElementById('team_name').value + " Win!!!";
+    blu.style.fontSize = "40px";
+    blu.style.textAlign = "center";
 
-    document.getElementById("main").appendChild(div);
+    document.getElementById("main").appendChild(blu);
 }
 
-function winlose(){
+function winlose() {
     var swingnum = document.getElementById('swing').value
     var pitchnum = document.getElementById('pitch').value
     var pitchdiff = Math.abs(swingnum - pitchnum)
     if (pitchdiff > 50) {
         var absdiff = 100 - pitchdiff
     } else {
-        var absdiff = pitchdiff  
+        var absdiff = pitchdiff
     }
     if (absdiff < 4) {
         document.getElementById('resultb').value = "HOME RUN"
@@ -92,16 +93,28 @@ function winlose(){
     } else if (absdiff < 11) {
         document.getElementById('resultb').value = "Single"
         youwin()
-    } else  if (absdiff < 41) {
+    } else if (absdiff < 41) {
         document.getElementById('resultb').value = "Flyout"
         yerout()
     } else {
         document.getElementById('resultb').value = "Strikeout!"
         yerout()
     }
-    
+
 }
 
+function unlock() {
+    document.getElementById('gameplay').disabled = false;
+    document.getElementById('howmanyouts').innerHTML = "X X O";
+    document.getElementById('homescore').innerHTML = "2";
+    if (red) {
+        red.remove()
+    }
+    if (blu) {
+        blu.remove()
+    }
+
+}
 
 
 
